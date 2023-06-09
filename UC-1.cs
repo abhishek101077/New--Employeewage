b558a8d6c6866833830cbf80601589fb4e3de431
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-06-10 00:36:47 +0530

    First use case

---
+++ UC-1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeewageRemap
+{
+    public class UC_1
+    {
+        public static void solution()
+        {
+            Console.WriteLine("Welcome to the employee wage problem");
+
+            int isfulltime = 1;
+            Random random
+                = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == isfulltime)
+            {
+
+                Console.WriteLine("The employee is present");
+
+            }
+            else 
+            {
+                Console.WriteLine("The employee is absent");
+            }
+
+
+        }
+    }
+}
