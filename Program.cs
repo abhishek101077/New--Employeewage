194eed7a6f30f090cd5dcac149de9ca7be8b45e4
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-06-10 00:32:32 +0530

    First programme file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeewageRemap
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,welcome to the Employee wage problem");
+            //UC_1.solution();
+            // UC_2.solution();
+            UC_6.solution();
+        }
+    }
+}
